cfddc896fa30fd8567d3520fe6da6bdce5cac87a
Devajayantha <devajayantha888@gmail.com>
2024-01-02 00:00:12 +0800

    + add annotation repository topic

--- src/main/java/com/devajayantha/main/models/repositories/TopicRepository.java
+++ src/main/java/com/devajayantha/main/models/repositories/TopicRepository.java
@@ -2,10 +2,12 @@ package com.devajayantha.main.models.repositories;
 
 import com.devajayantha.main.models.entities.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TopicRepository extends JpaRepository<Topic, Long> {
     List<Topic> findAll();
 
