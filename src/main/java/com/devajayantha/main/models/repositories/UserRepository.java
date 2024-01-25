1c0cac67c262377d1ee3d1249118058b584a38b7
Devajayantha <devajayantha888@gmail.com>
2024-01-25 18:07:40 +0800

    + add suser seeder

---
+++ src/main/java/com/devajayantha/main/models/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.devajayantha.main.models.repositories;
+
+import com.devajayantha.main.models.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/main/java/com/devajayantha/main/seeders/UserSeeder.java
@@ -0,0 +1,28 @@
+package com.devajayantha.main.seeders;
+
+import com.devajayantha.main.models.entities.User;
+import com.devajayantha.main.models.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserSeeder implements CommandLineRunner {
+    private final UserRepository repository;
+
+    public UserSeeder(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (repository.count() == 0) {
+            User entity = new User();
+            entity.setName("Admin");
+            entity.setEmail("admin@gmail.com");
+            entity.setRole("superadmin");
+            entity.setPassword("password");
+
+            repository.saveAndFlush(entity);
+        }
+    }
+}
